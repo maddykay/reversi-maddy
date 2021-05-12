1fdcf883f0fc31077fbbbb5db0558850c45300b9
Maddy Thomas <mkthomas@uci.edu>
2021-05-11 17:43:09 -0700

    Updates

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,6 +1,6 @@
 function getIRIParameterValue(requestedKey){
     let pageIRI = window.location.search.substring(1);
-    let pageIRIvariables = pageIRI.split('&');
+    let pageIRIVariables = pageIRI.split('&');
     for(let i = 0 ; i < pageIRIVariables.length; i++){
         let data = pageIRIVariables[i].split('=');
         let key = data[0];
@@ -16,4 +16,4 @@ if ((typeof username == 'undefined') || (username === null)){
     username = "Anonymous_"+Math.floor(Math.random()*1000);
 }
 
-$('#messages').prepend('<b>'+username+':</b>')
+$('#messages').prepend('<b>'+username+':</b>');
