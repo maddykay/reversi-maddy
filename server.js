aa08b2e558bd4d36dcc0d6967e7475c67a42879e
Maddy Thomas <mkthomas@uci.edu>
2021-05-11 22:17:09 -0700

    Socket updates

--- server.js
+++ server.js
@@ -10,22 +10,46 @@ let port = process.env.PORT;
 let directory = __dirname + '/public';
 
 /* If we aren't on Heruko, then we need to adjust our port and directory */
-if ((typeof port == 'undefined') || (port === null)){
+if ((typeof port == 'undefined') || (port === null)) {
     port = 8080;
-    directory = '.public/'
+    directory = './public';
 }
 
 /* Set up our static file web server to deliver files from the filesystem */
 let file = new static.Server(directory);
 
 let app = http.createServer(
-    function(request,response){
+    function (request, response) {
         request.addListener('end',
-            function(){
-                file.serve(request,response);
+            function () {
+                file.serve(request, response);
             }
         ).resume();
     }
 ).listen(port);
 
 console.log('The server is running');
+
+/*********************************/
+/* Set up the web socket server */
+
+const { Server } = require("socket.io");
+const io = new Server(app);
+
+io.on('connection', (socket) => {
+
+    /* Output a log message on the server and send it to the clients */
+    function serverLog(...messages){
+        io.emit('log',['**** Message from the server:\n']);
+        messages.forEach((item) => {
+            io.emit('log',['****\t'+item]);
+            console.log(item);
+        });
+    }
+
+    serverLog('a page connected to the server: '+socket.id);
+
+    socket.on('disconnect', () => {
+       serverLog('a page disconnected from the server: '+socket.id);
+    });
+});
